b5c35dd0729e0e45664b64e68abb39447f465a95
Mateusz Nowak <mnowakio@outlook.com>
2019-09-14 20:19:45 +0200

    basic line drawing

--- fe/src/app/components/destinations/map/map.component.ts
+++ fe/src/app/components/destinations/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, AfterViewInit } from "@angular/core";
+
 var mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
 
 @Component({
@@ -7,19 +8,21 @@ var mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
   styleUrls: ["./map.component.scss"]
 })
 export class MapComponent implements OnInit {
-
   map: any;
 
-  coords;
+  turf;
 
-  constructor() {}
+  constructor() {
+    this.turf = eval("turf");
+  }
 
-  addMarker(marker, className: string = 'marker') {
+  addMarker(marker, className: string = "marker") {
+    
     // create a HTML element for each feature
-    var el = document.createElement('app-marker');
+    var el = document.createElement("app-marker");
 
     el.className = className;
-  
+
     // make a marker for each feature and add to the map
     new mapboxgl.Marker(el)
       .setLngLat(marker.geometry.coordinates)
@@ -27,22 +30,125 @@ export class MapComponent implements OnInit {
 
     new mapboxgl.Marker(el)
       .setLngLat(marker.geometry.coordinates)
-      .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-        .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
+      .setPopup(
+        new mapboxgl.Popup({ offset: 25 }) // add popups
+          .setHTML(
+            "<h3>" +
+              marker.properties.title +
+              "</h3><p>" +
+              marker.properties.description +
+              "</p>"
+          )
+      )
       .addTo(this.map);
 
-      el.addEventListener('click', (e)=>{
-        // Prevent the `map.on('click')` from being triggered
-        e.stopPropagation();
-        if(className === "marker"){
-          console.log(marker);
-        }
-      });
+    el.addEventListener("click", e => {
+      // Prevent the `map.on('click')` from being triggered
+      e.stopPropagation();
+      if (className === "marker") {
+        console.log(marker);
+      }
+    });
   }
 
-  
+  drawLine() {
+    // San Francisco
+    var origin = [-122.414, 37.776];
+
+    // Washington DC
+    var destination = [-77.032, 38.913];
+
+    // A simple line from origin to destination.
+    var route = {
+      type: "FeatureCollection",
+      features: [
+        {
+          type: "Feature",
+          geometry: {
+            type: "LineString",
+            coordinates: [origin, destination]
+          }
+        }
+      ]
+    };
+
+    // A single point that animates along the route.
+    // Coordinates are initially set to origin.
+    var point = {
+      type: "FeatureCollection",
+      features: [
+        {
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "Point",
+            coordinates: origin
+          }
+        }
+      ]
+    };
+
+    // Calculate the distance in kilometers between route start/end point.
+    var lineDistance = this.turf.lineDistance(route.features[0], "kilometers");
+    console.log(lineDistance);
+
+    var arc = [];
+
+    // Number of steps to use in the arc and animation, more steps means
+    // a smoother arc and animation, but too many steps will result in a
+    // low frame rate
+    var steps = 500;
+
+    // Draw an arc between the `origin` & `destination` of the two points
+    for (var i = 0; i < lineDistance; i += lineDistance / steps) {
+      var segment = this.turf.along(route.features[0], i, "kilometers");
+      arc.push(segment.geometry.coordinates);
+    }
+
+    // Update the route with calculated arc coordinates
+    route.features[0].geometry.coordinates = arc;
+
+    // Used to increment the value of the point measurement against the route.
+    var counter = 0;
+
+    this.map.addSource("route", {
+      type: "geojson",
+      data: route
+    });
+
+    this.map.addSource("point", {
+      type: "geojson",
+      data: point
+    });
+
+    this.map.addLayer({
+      id: "route",
+      source: "route",
+      type: "line",
+      paint: {
+        "line-width": 2,
+        "line-color": "#007cbf"
+      }
+    });
+
+    this.map.addLayer({
+      id: "point",
+      source: "point",
+      type: "symbol",
+      layout: {
+        "icon-image": "airport-15",
+        "icon-rotate": ["get", "bearing"],
+        "icon-rotation-alignment": "map",
+        "icon-allow-overlap": true,
+        "icon-ignore-placement": true
+      }
+    });
+  }
 
   ngOnInit() {
+    console.log("eval turf");
+    console.log(eval("turf"));
+
     mapboxgl.accessToken =
       "pk.eyJ1IjoibW5vd2FraW8iLCJhIjoiY2swam84ZndwMGJyejNsbGkwcnB4eW93bSJ9.-NzyDwHjvusBINe7FLfSvw";
     this.map = new mapboxgl.Map({
@@ -51,35 +157,34 @@ export class MapComponent implements OnInit {
     });
 
     var geojson = {
-      type: 'FeatureCollection',
-      features: [{
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-77.032, 38.913]
+      type: "FeatureCollection",
+      features: [
+        {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [-77.032, 38.913]
+          },
+          properties: {
+            title: "Mapbox",
+            description: "Washington, D.C."
+          }
         },
-        properties: {
-          title: 'Mapbox',
-          description: 'Washington, D.C.'
+        {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [-122.414, 37.776]
+          },
+          properties: {
+            title: "Mapbox",
+            description: "San Francisco, California"
+          }
         }
-      },
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-122.414, 37.776]
-        },
-        properties: {
-          title: 'Mapbox',
-          description: 'San Francisco, California'
-        }
-      }]
+      ]
     };
 
-
-
-
-    this.map.on('load', ()=>{
+    this.map.on("load", () => {
       // this.map.addLayer(
       //   {
       //     id: "cities",
@@ -113,17 +218,16 @@ export class MapComponent implements OnInit {
       //   }
       // });
 
-
-
-
       let markers = [];
       const ownIndex = 1;
       geojson.features.forEach((marker, index) => {
-        markers.push(this.addMarker(marker, (index === ownIndex) ? "marker-start" : "marker"));
+        markers.push(
+          this.addMarker(marker, index === ownIndex ? "marker-start" : "marker")
+        );
       });
 
-
-
+      // draw line
+      this.drawLine();
     });
   }
 }
